83f05c594298eff5c116ddc84f312d554e76fce4
Samuele Santi <redshadow@hackzine.org>
2013-01-12 10:46:19 +0100

    Now symlinking hook script instead of copying over

--- fix-postreceive-hook.sh
+++ fix-postreceive-hook.sh
@@ -5,6 +5,6 @@ HOOKFILE=/home/git/.gitolite/hooks/common/post-receive
 for repo in /home/git/repositories/*.git; do
     if [ ! -e "$repo"/hooks/post-receive ]; then
 	echo "Repository ${repo} is missing hook script -> fixing"
-	cp "$HOOKFILE" "$repo"/hooks/post-receive
+	ln -s "$HOOKFILE" "$repo"/hooks/post-receive
     fi
 done
